446d8512fe9df8ae041863c34156259031a88dab
melvincwng <melvincwng@users.noreply.github.com>
2021-03-08 15:48:39 +0800

    Implement User routes

--- src/app.js
+++ src/app.js
@@ -2,5 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const userRouter = require("./routes/users.routes");
+app.use("/users", userRouter);
 
 module.exports = app;
---
+++ src/routes/users.routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+
+router.post("/", async (req, res, next) => {
+    try {
+      const user = new User(req.body);
+      const newUser= await user.save();
+      res.send(newUser); // default status code is 200, if successful POST request
+    } catch (err) {
+      next(err);
+    }
+  });
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const result = await bcrypt.compare(password, user.password);
+
+    if (!result) {
+      throw new Error("Login failed");
+    }
+
+    res.send("You are now logged in!"); 
+  } catch (err) {
+    if (err.message === "Login failed") {
+      err.statusCode = 400;
+    }
+    next(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  res.send("You are now logged out!");
+});
+
+module.exports = router;
+  
