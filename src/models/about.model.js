9b6fd264dfe26886d5a87a5f318d47a80f5bd6a4
melvincwng <melvincwng@users.noreply.github.com>
2021-03-09 16:31:58 +0800

    Implement About Project model

---
+++ src/models/about.model.js
@@ -0,0 +1,19 @@
+//about.model.js
+const mongoose = require("mongoose");
+
+const aboutSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+  body: {
+    type: String,
+    required: true,
+    minlength: 1,
+  },
+});
+
+const About = mongoose.model("About", aboutSchema);
+
+module.exports = About;
