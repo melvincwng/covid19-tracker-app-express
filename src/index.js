e18b89be64c41317ceb71ce5e1307a917524e716
melvincwng <melvincwng@users.noreply.github.com>
2021-03-11 11:46:15 +0800

    Update code for PORT for heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require("./utils/db");
 const app = require("./app");
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Express app started on http://localhost:${PORT}`);
