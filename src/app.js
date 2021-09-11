02392558379b252de0a4bf02452d3328061181b7
melvincwng <melvincwng@users.noreply.github.com>
2021-09-12 01:41:13 +0800

    Made express-app aka backend server host/serve images

--- src/app.js
+++ src/app.js
@@ -5,14 +5,28 @@ const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 require("dotenv").config();
 
-const cors = require('cors');
+const cors = require("cors");
 
 var corsOptions = {
-  origin: [process.env.FRONTEND_ONE_URL, process.env.FRONTEND_TWO_URL, process.env.FRONTEND_THREE_URL, process.env.FRONTEND_FOUR_URL, "http://localhost:3000", "http://localhost:3000/login", "http://localhost:3000/articles", "http://localhost:3000/about"],
+  origin: [
+    process.env.FRONTEND_ONE_URL,
+    process.env.FRONTEND_TWO_URL,
+    process.env.FRONTEND_THREE_URL,
+    process.env.FRONTEND_FOUR_URL,
+    "http://localhost:3000",
+    "http://localhost:3000/login",
+    "http://localhost:3000/articles",
+    "http://localhost:3000/about",
+  ],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+
+// 12/09/21: To serve static files like images in the 'images' folder of the express app
+// https://expressjs.com/en/starter/static-files.html
+// Hence in localhost for instance, the images will be hosted at e.g. localhost:3001/images/test.jpg
+app.use("/images", express.static("images"));
 
 const userRouter = require("./routes/users.routes");
 const articleRouter = require("./routes/articles.routes");
@@ -27,5 +41,5 @@ app.use((err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     res.status(err.statusCode).send(err.message);
   });
-  
+
 module.exports = app;
